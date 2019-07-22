c1d55304a72f44af5131d6b2c0039e60ffa75a67
Punit Jain <punit.jain@hbo.com>
2019-07-22 11:37:02 +0530

    Added bootstrap for ec2

---
+++ bootstrap-linux.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+yum update -y
+yum install httpd -y
+service httpd start
+chkconfig httpd on
+cd /var/www/html
+echo "<html><h1>Hello AWS World – running on Linux – on port 80</h1></html>" > index.html
